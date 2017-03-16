e4825fcfad83fb8d452c898888fadf66bf604b53
MPELLETIERLT\mpelletier <matthewbpelletier@gmail.com>
2017-03-16 17:09:15 -0400

    Prefabbed pins Working on basic physics Starting UI with touch inputs

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	public Vector3 launchVelocity;
+
+	private AudioSource audioSource;
+	private Rigidbody rigidb;
+
+	// Use this for initialization
+	void Start () {
+
+		rigidb = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidb.velocity = launchVelocity;
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
